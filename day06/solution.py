d7e0ff78d7dcaf79882cd8d9f7cf48c49ade0210
Aeilko Bos <aeilkobos@hotmail.com>
2022-12-06 16:21:43 +0100

    Day 6

---
+++ day06/solution.py
@@ -0,0 +1,74 @@
+from utils.aoc import check_for_input_file
+from utils.file import read_file_content
+
+
+def solve_part1(input: str) -> int:
+    line = input[:-1]
+    r = 0
+    chars = []
+    for i in range(len(line)):
+        c = line[i]
+        if c in chars:
+            idx = chars.index(c)
+            chars = chars[idx+1:]
+        chars.append(c)
+        if len(chars) == 4:
+            r = i+1
+            break
+
+    return r
+
+
+def solve_part2(input: str) -> int:
+    line = input[:-1]
+    r = 0
+    chars = []
+    for i in range(len(line)):
+        c = line[i]
+        if c in chars:
+            idx = chars.index(c)
+            chars = chars[idx+1:]
+        chars.append(c)
+        if len(chars) == 14:
+            r = i+1
+            break
+
+    return r
+
+
+def test_part1():
+    input = read_file_content("inputs/test")
+    answer = int(read_file_content("inputs/ans1"))
+
+    result = solve_part1(input)
+    if result == answer:
+        print("Test successful")
+    else:
+        print("Test unsuccessful: " + str(result) + ", expected: " + str(answer))
+
+
+def test_part2():
+    input = read_file_content("inputs/test")
+    answer = int(read_file_content("inputs/ans2"))
+
+    result = solve_part2(input)
+    if result == answer:
+        print("Test successful")
+    else:
+        print("Test unsuccessful: " + str(result) + ", expected: " + str(answer))
+
+
+if __name__ == '__main__':
+    # Check if we have an input file, if not, download it
+    check_for_input_file()
+
+    # Perform this day's solution
+    input = read_file_content("inputs/input")
+
+    print(" --- Part 1 --- ")
+    test_part1()
+    print("Part 1 result:\t" + str(solve_part1(input)))
+
+    print("\n --- Part 2 ---")
+    test_part2()
+    print("Part 2 result:\t" + str(solve_part2(input)))
